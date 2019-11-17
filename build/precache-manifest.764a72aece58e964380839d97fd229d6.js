dafaee1d90ec5efa1d396cb56b339d8ad83f382e
Raine Rantanen <raine.ran7anen@gmail.com>
2019-11-17 14:02:39 +0200

    uibuild

---
+++ build/precache-manifest.764a72aece58e964380839d97fd229d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e47f2399db092f75b788e12c769eb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e437e165d3c1bb2c7d52",
+    "url": "/static/css/main.b0f19b03.chunk.css"
+  },
+  {
+    "revision": "a0210ab1b5b72d3108e6",
+    "url": "/static/js/2.09ccb4a8.chunk.js"
+  },
+  {
+    "revision": "e437e165d3c1bb2c7d52",
+    "url": "/static/js/main.e87530e3.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
